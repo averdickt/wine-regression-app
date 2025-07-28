27dcc3d955e740e5e9b60e9f815615d1c13bc5f4
averdickt <the.verdickts@gmail.com>
2025-07-28 21:39:29 +0100

    Update WineDataDisplay.js

--- src/components/WineDataDisplay.js
+++ src/components/WineDataDisplay.js
@@ -1,19 +1,47 @@
 // src/components/WineDataDisplay.js
 import React, { useMemo } from "react";
-import { calculateRegression, calculatePremiumDiscount } from "../utils/regression";
-import "../styles/WineDataDisplay.css";
+import { wineData } from "../utils/data";
 
-const WineDataDisplay = ({ filters, filteredData }) => {
-  // Only display if a specific vintage is selected
-  if (filters.vintage === "all") {
-    return null;
-  }
+const WineDataDisplay = ({ filters }) => {
+  const filteredData = useMemo(() => {
+    let data = wineData;
+    if (filters.wine !== "all") data = data.filter((d) => d.wine === filters.wine);
+    if (filters.vintage !== "all") data = data.filter((d) => d.vintage === filters.vintage);
+    if (filters.region !== "all") data = data.filter((d) => d.region === filters.region);
+    if (filters.score !== "all") data = data.filter((d) => d.score === Number(filters.score));
+    if (filters.wineClass !== "all") data = data.filter((d) => d.wineClass === filters.wineClass);
+    return data;
+  }, [filters]);
 
-  // Find the selected wine based on vintage
-  const selectedWine = useMemo(() => {
-    return filteredData.find((d) => d.vintage === Number(filters.vintage));
-  }, [filteredData, filters.vintage]);
+  return (
+    <div>
+      <h3>Wine Data</h3>
+      <table>
+        <thead>
+          <tr>
+            <th>Wine</th>
+            <th>Vintage</th>
+            <th>Region</th>
+            <th>Score</th>
+            <th>Wine Class</th>
+            <th>Price</th>
+          </tr>
+        </thead>
+        <tbody>
+          {filteredData.map((item, index) => (
+            <tr key={index}>
+              <td>{item.wine}</td>
+              <td>{item.vintage}</td>
+              <td>{item.region}</td>
+              <td>{item.score}</td>
+              <td>{item.wineClass}</td>
+              <td>{item.price}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
 
-  // Calculate regression for the filtered data
-  const regression = useMemo(() => {
-    return calculateRegression(filteredData,
+export default WineDataDisplay;
