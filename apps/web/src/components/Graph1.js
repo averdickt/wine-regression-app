a1d60cf68e0f16e7867b8a884971fec7c7f3cf5e
averdickt <the.verdickts@gmail.com>
2025-08-15 16:59:03 +0100

    Update Graph1.js

--- apps/web/src/components/Graph1.js
+++ apps/web/src/components/Graph1.js
@@ -1,1 +1,43 @@
+import React from 'react';
+import {
+  ScatterChart, Scatter, XAxis, YAxis, CartesianGrid, Tooltip, Legend, Line
+} from 'recharts';
+import { wineData } from '../utils/data';
+import { filterWineData, calculateRegression } from '../utils/regression';
 
+const Graph1 = ({ selectedProduct }) => {
+  // Filter for only the product we care about
+  const productData = filterWineData(wineData, selectedProduct);
+
+  // Calculate regression from the score vs price
+  const regressionPoints = calculateRegression(productData, 'score', 'price');
+
+  return (
+    <ScatterChart
+      width={800}
+      height={500}
+      margin={{ top: 20, right: 20, bottom: 20, left: 20 }}
+    >
+      <CartesianGrid />
+      <XAxis type="number" dataKey="score" name="Score" />
+      <YAxis type="number" dataKey="price" name="Price" />
+      <Tooltip cursor={{ strokeDasharray: '3 3' }} />
+      <Legend />
+
+      {/* Scatter plot of actual data points */}
+      <Scatter name={selectedProduct} data={productData} fill="#8884d8" />
+
+      {/* Regression line */}
+      <Line
+        type="monotone"
+        data={regressionPoints}
+        dataKey="y"
+        stroke="#ff7300"
+        dot={false}
+        isAnimationActive={false}
+      />
+    </ScatterChart>
+  );
+};
+
+export default Graph1;
