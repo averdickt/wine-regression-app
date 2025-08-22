1c80232b90fb99a30bfd76278d64f56014b1946d
averdickt <the.verdickts@gmail.com>
2025-08-22 16:36:15 +0100

    Update regression.js

--- apps/web/src/lib/regression.js
+++ apps/web/src/lib/regression.js
@@ -1,1 +1,12 @@
+import regression from "regression";
 
+export function bestFitRegression(data) {
+  // data format: [[x, y], [x, y], ...]
+  const linear = regression.linear(data);
+  const polynomial = regression.polynomial(data, { order: 2 });
+
+  // Compare R^2 values
+  const betterFit = polynomial.r2 > linear.r2 ? polynomial : linear;
+
+  return betterFit;
+}
