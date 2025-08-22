f0732413f7fe20c3b55dd6b39ee1bbdc93c258ab
averdickt <the.verdickts@gmail.com>
2025-08-22 17:36:42 +0100

    Update AutocompleteBox.js

--- apps/web/src/components/AutocompleteBox.js
+++ apps/web/src/components/AutocompleteBox.js
@@ -1,1 +1,21 @@
+import React from "react";
 
+export default function AutocompleteBox({ options, value, onChange }) {
+  return (
+    <div>
+      <input
+        type="text"
+        list="product-options"
+        value={value}
+        onChange={(e) => onChange(e.target.value)}
+        placeholder="Select product..."
+        style={{ width: "300px", padding: "6px" }}
+      />
+      <datalist id="product-options">
+        {options.map((opt) => (
+          <option key={opt} value={opt} />
+        ))}
+      </datalist>
+    </div>
+  );
+}
