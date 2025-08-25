6dfaa8aafb1b38611f5571f0a4261a5c46a5c817
averdickt <the.verdickts@gmail.com>
2025-08-25 12:31:02 +0100

    Update PriceScoreVintageChart.js

--- apps/web/src/components/PriceScoreVintageChart.js
+++ apps/web/src/components/PriceScoreVintageChart.js
@@ -43,8 +43,8 @@ export default function PriceScoreVintageChart({ data, highlightVintage, DA_Star
             <circle
               cx={cx}
               cy={cy}
-              r={highlightVintage && string(payload.Vintage) === string(highlightVintage) ? 8 : 4}
-              fill={highlightVintage && strin(payload.Vintage) === string(highlightVintage) ? "red" : "black"}
+              r={highlightVintage && String(payload.Vintage) === String(highlightVintage) ? 8 : 4}
+              fill={highlightVintage && String(payload.Vintage) === String(highlightVintage) ? "red" : "black"}
             />
           );
         }}
