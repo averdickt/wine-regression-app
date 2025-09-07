bf23fc0604820ef5989b53a77db050df8762d8ac
averdickt <the.verdickts@gmail.com>
2025-09-07 15:59:49 +0100

    Update Dropdown.js

--- apps/web/src/components/Dropdown.js
+++ apps/web/src/components/Dropdown.js
@@ -1,11 +1,17 @@
 import React from "react";
 
-export default function Dropdown({ options, value, onChange }) {
+export default function Dropdown({ options, value, onChange, placeholder }) {
   return (
-    <select value={value} onChange={(e) => onChange(e.target.value)}>
-      <option value="">-- Select Vintage --</option>
+    <select
+      value={value}
+      onChange={(e) => onChange(e.target.value)}
+      style={{ padding: "6px", fontSize: "14px" }}
+    >
+      <option value="">{placeholder || "-- Select an option --"}</option>
       {options.map((opt) => (
-        <option key={opt} value={opt}>{opt}</option>
+        <option key={opt} value={opt}>
+          {opt}
+        </option>
       ))}
     </select>
   );
