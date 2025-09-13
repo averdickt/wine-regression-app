76438512c1ef313760d605e7a6cc9ffb5f3fc9d2
averdickt <the.verdickts@gmail.com>
2025-09-13 21:59:57 +0100

    Update PriceScoreVintageChart.js

--- apps/web/src/components/PriceScoreVintageChart.js
+++ apps/web/src/components/PriceScoreVintageChart.js
@@ -14,10 +14,6 @@ export default function PriceScoreVintageChart({ data, highlightVintage, colorMa
   if (!data || data.length === 0) return <p>No data available</p>;
 
   const highlightRow = data.find((r) => r.Vintage === highlightVintage);
-  const startYear = highlightRow?.DA_Start || highlightVintage;
-  const endYear = highlightRow?.DA_Finish || highlightVintage;
-
-  // Chart domains
   const minVintage = Math.min(...data.map((d) => d.Vintage));
   const maxVintage = Math.max(...data.map((d) => d.Vintage));
 
@@ -29,30 +25,26 @@ export default function PriceScoreVintageChart({ data, highlightVintage, colorMa
         <YAxis type="number" dataKey="Price" />
         <Tooltip cursor={{ strokeDasharray: "3 3" }} />
 
-        {/* Background ranges */}
         {highlightRow && (
           <>
-            {/* Before DA_Start = red */}
             <ReferenceArea
               x1={minVintage}
-              x2={startYear}
+              x2={highlightRow.DA_Start}
               y1={0}
               y2="100%"
               fill={colorMap.red}
               fillOpacity={0.2}
             />
-            {/* Between start and finish = green */}
             <ReferenceArea
-              x1={startYear}
-              x2={endYear}
+              x1={highlightRow.DA_Start}
+              x2={highlightRow.DA_Finish}
               y1={0}
               y2="100%"
               fill={colorMap.green}
               fillOpacity={0.2}
             />
-            {/* After DA_Finish = yellow */}
             <ReferenceArea
-              x1={endYear}
+              x1={highlightRow.DA_Finish}
               x2={maxVintage}
               y1={0}
               y2="100%"
@@ -62,21 +54,8 @@ export default function PriceScoreVintageChart({ data, highlightVintage, colorMa
           </>
         )}
 
-        {/* All data points */}
-        <Scatter
-          name="Wines"
-          data={data}
-          fill="#8884d8"
-        />
-
-        {/* Highlighted vintage */}
-        {highlightRow && (
-          <Scatter
-            name="Selected Vintage"
-            data={[highlightRow]}
-            fill="#FF0000"
-          />
-        )}
+        <Scatter data={data} fill="#8884d8" />
+        {highlightRow && <Scatter data={[highlightRow]} fill="#FF0000" />}
       </ScatterChart>
     </ResponsiveContainer>
   );
