608826293411e4d4a2398575f71bdf61f147dfc1
averdickt <the.verdickts@gmail.com>
2025-09-21 11:24:39 +0100

    Create index.js

---
+++ packages/shared-logic/index.js
@@ -0,0 +1,4 @@
+// Export all shared utilities
+export { calculateRegression } from './regression';
+export { parseWineData } from './excelParser';
+export { calculateRelativeValue } from './relativeValue';
